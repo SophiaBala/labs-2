29e530e5d35caa6ea410d9e18491085df93678a6
Sofia Bala <114072833+SophiaBala@users.noreply.github.com>
2025-03-05 11:18:05 +0200

    Update additional-task.py

--- additional-task.py
+++ additional-task.py
@@ -43,11 +43,31 @@ def zigzag(matrix):
 
     return result  
 
-matrix = [
-    [7, 6, 2, 1],
-    [13, 8, 5, 3],
-    [14, 12, 9, 4],
-    [16, 15, 11, 10]
-]
+class TestZigzag(unittest.TestCase):
 
-print(zigzag(matrix))
+    def test_matrix(self):
+        matrix2x4 = [
+            [7, 6, 2, 1],
+            [13, 8, 5, 3],
+            [14, 12, 9, 4]
+        ]
+
+        expected = [1,2,3,4,5,6,7,8,9,12,13,14]
+        result = zigzag(matrix2x4)
+        self.assertEqual(expected, result)
+
+    def test_matrix5x5(self):
+        matrix = [
+            [1,2,3,4,5],
+            [6,7,8,9,10],
+            [11,12,13,14,15],
+            [16,17,18,19,20],
+            [21,22,23,24,25]
+        ]
+
+        expected = [5,4,10,15,9,3,2,8,14,20,25,19,13,7,1,6,12,18,24,23,17,11,16,22,21]
+        result = zigzag(matrix)
+        self.assertEqual(expected, result)
+
+if __name__ == '__main__':
+    unittest.main()
