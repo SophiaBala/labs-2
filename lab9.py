89a748b45a4124881e9abbf6d42e1972a9c8ae6b
Sofia Bala <114072833+SophiaBala@users.noreply.github.com>
2025-05-25 21:52:50 +0300

    Add files via upload

---
+++ lab9.py
@@ -0,0 +1,51 @@
+with open("tiles.txt", "r") as f:
+    lines = f.read().splitlines()
+
+W, H = map(int, lines[0].split())
+
+grid = []
+for i in range(1, H + 1):
+    grid.append(lines[i])
+
+ways = []
+for y in range(H):
+    row = []
+    for x in range(W):
+        row.append(0)
+    ways.append(row)
+
+
+for y in range(H):
+    ways[y][0] = 1
+
+
+for x in range(W - 1):
+    for y in range(H):
+        value = ways[y][x]
+        if value == 0:
+            continue
+
+        current_letter = grid[y][x]
+
+        ways[y][x + 1] += value
+
+        if y > 0 and grid[y - 1][x + 1] == current_letter:
+            ways[y - 1][x + 1] += value
+
+        if y < H - 1 and grid[y + 1][x + 1] == current_letter:
+            ways[y + 1][x + 1] += value
+
+        for xx in range(x + 2, W):
+            if grid[y][xx] == current_letter:
+                ways[y][xx] += value
+
+print("Ways:")
+for row in ways:
+    print(row)
+
+result = 0
+if H == 1:
+    result = ways[0][W - 1]
+else:
+    result = ways[0][W - 1] + ways[H - 1][W - 1]
+print("Amount of ways:", result)
